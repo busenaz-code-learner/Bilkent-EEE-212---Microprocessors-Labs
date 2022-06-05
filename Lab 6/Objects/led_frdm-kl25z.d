1d0efb015f9e18f883f0abca5875c2c9e6534bfe
Busenaz Kerimgil <77046992+busenaz-code-learner@users.noreply.github.com>
2022-06-05 16:54:47 +0300

    Add files via upload

---
+++ Lab 6/Objects/led_frdm-kl25z.d
@@ -0,0 +1,9 @@
+.\objects\led_frdm-kl25z.o: C:\Users\hp\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Boards\NXP\FRDM-KL25Z\Common\LED_FRDM-KL25Z.c
+.\objects\led_frdm-kl25z.o: C:\Users\hp\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\objects\led_frdm-kl25z.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\led_frdm-kl25z.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\led_frdm-kl25z.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\led_frdm-kl25z.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\led_frdm-kl25z.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\led_frdm-kl25z.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
+.\objects\led_frdm-kl25z.o: C:\Users\hp\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.13.0\Board\Board_LED.h
---
+++ Lab 6/Objects/sample_adc.d
@@ -0,0 +1,8 @@
+.\objects\sample_adc.o: sample_ADC.c
+.\objects\sample_adc.o: C:\Users\hp\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\objects\sample_adc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\sample_adc.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\sample_adc.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\sample_adc.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\sample_adc.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\sample_adc.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
