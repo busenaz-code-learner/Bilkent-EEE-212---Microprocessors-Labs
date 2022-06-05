9c39a0b313e0b90759a6d82173592431b989f0c5
Busenaz Kerimgil <77046992+busenaz-code-learner@users.noreply.github.com>
2022-06-05 16:49:28 +0300

    Add files via upload

---
+++ Lab 4/Chapter3/Objects/lab 5.d
@@ -0,0 +1,11 @@
+.\objects\lab 5.o: ..\..\Lab 5\Lab 5.c
+.\objects\lab 5.o: C:\Users\hp\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.H
+.\objects\lab 5.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\lab 5.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\lab 5.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\lab 5.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\lab 5.o: C:\Users\hp\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\lab 5.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
+.\objects\lab 5.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\lab 5.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\lab 5.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
